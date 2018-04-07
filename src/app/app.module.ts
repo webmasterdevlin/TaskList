2259b596216d1b64e8ba7159e37dfa9924ff49b2
webmasterdevlin <webmasterdevlin@gmail.com>
2018-04-07 18:50:26 +0800

    Used Angular Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,60 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { TaskService } from "./services/task.service";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule, MatDatepickerModule, MatDialogModule, MatFormField, MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule, MatMenuModule, MatNativeDateModule,
+  MatProgressBarModule, MatRadioModule,
+  MatSelectModule, MatSnackBarModule,
+  MatToolbarModule
+} from '@angular/material';
+import {CommonModule} from '@angular/common';
+import { AppRoutingModule } from './/app-routing.module';
+import { NewTaskComponent } from './pages/new-task.component';
+import { EditTaskComponent } from './pages/edit-task.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NewTaskComponent, EditTaskComponent, HomeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatListModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatNativeDateModule,
+    MatMenuModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

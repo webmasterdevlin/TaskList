20367d36aae9b12bc4fd6afa12b40f658171071a
webmasterdevlin <webmasterdevlin@gmail.com>
2018-04-10 21:26:58 +0800

    Added focus event in the calendar picker and created mat-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,21 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { LocalTaskService } from "./services/local-task.service";
 import { HttpClientModule } from "@angular/common/http";
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { FlexLayoutModule } from "@angular/flex-layout";
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatFormFieldModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatProgressBarModule, MatProgressSpinnerModule,
-  MatRadioModule,
-  MatSelectModule, MatSlideToggleModule,
-  MatSnackBarModule,
-  MatToolbarModule
-} from '@angular/material';
+
 import { CommonModule } from "@angular/common";
 import { AppRoutingModule } from ".//app-routing.module";
-import { NewTaskComponent } from "./pages/new-task.component";
-import { EditTaskComponent } from "./pages/edit-task.component";
+import { NewTaskComponent } from "./pages/new-task/new-task.component";
+import { EditTaskComponent } from "./pages/edit-task/edit-task.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HomeComponent } from "./pages/home.component";
+import { HomeComponent } from "./pages/home/home.component";
 import { IsDonePipe } from "./pipes/is-done.pipe";
 
-import {AngularFireDatabaseModule} from 'angularfire2/database-deprecated';
-import {AngularFireModule} from 'angularfire2';
-import {FirebaseTaskService} from './services/firebase-task.service';
+import { AngularFireDatabaseModule } from "angularfire2/database-deprecated";
+import { AngularFireModule } from "angularfire2";
+import { FirebaseTaskService } from "./services/firebase-task.service";
+import { MatComponentsModule } from "./mat-components/mat-components.module";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAb-QD_jDPPA4mM0a8L_i7dp2sLbuz_YBI",
@@ -55,35 +38,16 @@ const firebaseConfig = {
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CommonModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
     HttpClientModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
-    FormsModule,
-    MatButtonModule,
-    FlexLayoutModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatDatepickerModule,
-    MatCardModule,
-    MatInputModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatGridListModule,
-    MatListModule,
-    MatProgressBarModule,
-    MatSnackBarModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatDialogModule,
-    MatCheckboxModule,
-    MatNativeDateModule,
-    MatMenuModule,
-    MatProgressSpinnerModule,
-    ReactiveFormsModule,
-    MatSlideToggleModule,
-    AppRoutingModule
+    MatComponentsModule
   ],
   providers: [LocalTaskService, FirebaseTaskService],
   bootstrap: [AppComponent]
